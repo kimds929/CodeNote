ed0cfa443d70976a1a1dd79a6c8185e978c4a704
kimds929 <kimds929@gmail.com>
2023-06-05 23:30:41 +0900

    Update Code
    .

--- 75_GenerativeModel/Generative21_GAN.py
+++ 75_GenerativeModel/Generative21_GAN.py
@@ -131,7 +131,7 @@ class GAN_Discriminator(torch.nn.Module):
 
 # sample_X = torch.rand(5,1,28,28)
 # gd = GAN_Discriminator()
-# gd(sample_X).shape
+# gd(sample_X)
 # gd(gen_image)
 
 
@@ -165,6 +165,8 @@ mdl_discriminator = GAN_Discriminator().to(device)
 # loss function & optimizer
 loss_function = torch.nn.BCELoss()
 
+k = 3     # hyper-parameter of number of iteration about Discriminator
+
 lr = 1e-4
 beta1 = 0.5
 optimizer_gen = torch.optim.RMSprop(mdl_generator.parameters(), lr=lr)
@@ -180,39 +182,43 @@ train_losses = []
 valid_losses = []
 fixed_noise = torch.randn(1, 100).to(device)  # 고정된 z를 사용해 생성된 이미지를 저장하기 위해 사용
 fixed_noise_images = []
+
 for e in range(n_epochs):
     start_time = time.time() # 시작 시간 기록
     
     mdl_discriminator.train()
     mdl_generator.train()
     train_epoch_loss = []
+    
     for batch_X, batch_y in train_loader:
-        # [Discriminator] --------------------------------------------------------------
-        optimizer_dis.zero_grad()      # optimizer initialize
-        
-        # (Objective Funciton)  min_θ max_Φ E[log{D_Φ(x)}] + E[log{1-D_Φ(G_θ(z))}]
-        
-        # discriminator forward
-        output_real = mdl_discriminator(batch_X.to(device))
-        label_true = torch.ones_like(output_real).to(device)
-        loss_real = loss_function(output_real, label_true)
-        
-        # generator forward
         noise_X = torch.randn([batch_X.shape[0], 100]).to(device)   # noise
         gen_X = mdl_generator(noise_X)
-        output_fake = mdl_discriminator(gen_X.detach())
-        label_fake = torch.zeros_like(output_fake).to(device)
-        loss_fake = loss_function(output_fake, label_fake)
-        
-        loss_dis = loss_real + loss_fake
-        
-        loss_dis.backward()         # backward
-        optimizer_dis.step()        # weight update
-        
+
+        for _ in range(k):
+            # [Discriminator] --------------------------------------------------------------
+            optimizer_dis.zero_grad()      # optimizer initialize
+            
+            # (Objective Funciton)  min_θ max_Φ E[log{D_Φ(x)}] + E[log{1-D_Φ(G_θ(z))}]
+            
+            # discriminator forward
+            output_real = mdl_discriminator(batch_X.to(device))
+            label_true = torch.ones_like(output_real).to(device)
+            loss_real = loss_function(output_real, label_true)
+            
+            # generator forward
+            output_fake = mdl_discriminator(gen_X.detach())
+            label_fake = torch.zeros_like(output_fake).to(device)
+            loss_fake = loss_function(output_fake, label_fake)
+            
+            loss_dis = loss_real + loss_fake
+            
+            loss_dis.backward()         # backward
+            optimizer_dis.step()        # weight update
+    
         # [Generator] --------------------------------------------------------------
         optimizer_gen.zero_grad()      # optimizer initialize
+
         output_fake = mdl_discriminator(gen_X)
-        
         loss_gen = loss_function(output_fake, label_true)
         
         loss_gen.backward()
@@ -340,9 +346,10 @@ def image_map(noises=None, gen_outputs=None, generator=None, n_w=8, digit_h=28, 
         
     return gen_map
 
-np.stack(map(lambda x: x.squeeze().to('cpu').numpy(), fixed_noise_images))
+gen_outputs = np.stack(map(lambda x: x.squeeze().to('cpu').numpy(), fixed_noise_images))
 
-plt.imshow(image_map(gen_outputs=fixed_noise_images), 'gray')
+
+plt.imshow(image_map(gen_outputs=gen_outputs), 'gray')
 plt.show()
 
 
@@ -366,153 +373,3 @@ plt.show()
 # Convolution Output Size ################
 # O = (Size - Kernel(Filter) + 2 * Padding)/Stride + 1
 #############################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
-from torchvision.transforms import transforms
-from tqdm import tqdm
-import os
-
-# # MNIST 데이터 로딩
-# transform = transforms.Compose([
-#     transforms.ToTensor(),
-#     transforms.Normalize((0.5,), (0.5,))
-# ])
-# trainset = MNIST(root='data', train=True, download=True, transform=transform)
-# dataloader = DataLoader(trainset, batch_size=128, shuffle=True)
-
-# # GPU 사용 가능하면 사용
-# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-# 생성자 모델 정의
-class Generator(torch.nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
-
-        self.model = torch.nn.Sequential(
-            torch.nn.Linear(100, 7*7*256, bias=False),
-            torch.nn.BatchNorm1d(7*7*256),
-            torch.nn.LeakyReLU(),
-
-            torch.nn.Unflatten(1, (256, 7, 7)),
-
-            torch.nn.ConvTranspose2d(256, 128, kernel_size=5, stride=1, padding=2, bias=False),
-            torch.nn.BatchNorm2d(128),
-            torch.nn.LeakyReLU(),
-
-            torch.nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
-            torch.nn.BatchNorm2d(64),
-            torch.nn.LeakyReLU(),
-
-            torch.nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1, bias=False),
-            torch.nn.Tanh()
-        )
-
-    def forward(self, z):
-        img = self.model(z)
-        return img
-
-# 판별자 모델 정의
-class Discriminator(torch.nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-
-        self.model = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1, bias=False),
-            torch.nn.LeakyReLU(negative_slope=0.2),
-
-            torch.nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
-            torch.nn.BatchNorm2d(128),
-            torch.nn.LeakyReLU(negative_slope=0.2),
-
-            torch.nn.Flatten(),
-            torch.nn.Linear(7*7*128, 1),
-            torch.nn.Sigmoid()
-        )
-
-    def forward(self, img):
-        pred = self.model(img)
-        return pred
-
-# 모델 초기화
-generator = Generator().to(device)
-discriminator = Discriminator().to(device)
-
-# 손실함수는 BCELoss 사용
-criterion = torch.nn.BCELoss()
-
-# Adam 최적화 알고리즘 사용
-lr = 2e-4
-beta1 = 0.5
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=lr, betas=(beta1, 0.999))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(beta1, 0.999))
-
-# 학습 시작
-num_epochs = 30
-
-fixed_noise = torch.randn(1, 100).to(device)  # 고정된 z를 사용해 생성된 이미지를 저장하기 위해 사용
-fixed_noise_images = []
-for epoch in range(1, num_epochs+1):
-    for idx, (images, _) in enumerate(train_loader):
-        real_images = images.to(device)
-
-        # 판별자 학습
-        # 진짜 데이터에 대한 판별 결과
-        true_labels = torch.ones(real_images.size(0), 1, device=device)  
-        true_outputs = discriminator(real_images).detach()
-        true_loss = criterion(true_outputs, true_labels)
-
-        # 가짜 데이터에 대한 판별 결과
-        noise = torch.randn(real_images.size(0), 100, device=device)  
-        fake_images = generator(noise)
-        fake_labels = torch.zeros(real_images.size(0), 1, device=device)  
-        fake_outputs = discriminator(fake_images.detach())
-        fake_loss = criterion(fake_outputs, fake_labels)
-
-        # 총 판별자 손실
-        d_loss = true_loss + fake_loss
-
-        optimizer_D.zero_grad()
-        d_loss.backward()
-        optimizer_D.step()
-
-        # 생성자 학습
-        # 가짜 데이터에 대한 판별 결과
-        fake_outputs = discriminator(fake_images)
-        g_loss = criterion(fake_outputs, true_labels)
-
-        optimizer_G.zero_grad()
-        g_loss.backward()
-        optimizer_G.step()
-
-    # 중간 과정 출력
-    # if epoch % 10 == 0:
-    print(f"[Epoch {epoch:3}] D_loss: {d_loss.item():6.4f}, G_loss: {g_loss.item():6.4f}")
-
-    # 고정된 z로 이미지 생성 후 저장
-    with torch.no_grad():
-        generator.eval()
-        fixed_generate_image = generator(fixed_noise)
-        fixed_noise_images.append(fixed_generate_image)
-
-
