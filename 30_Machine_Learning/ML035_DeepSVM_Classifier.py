790e3dee1edba36b0903aa8eafc9bb3e4620af34
kimds929 <kimds929@gmail.com>
2026-01-13 23:54:09 +0900

    Update
    .

--- 30_Machine_Learning/ML035_DeepSVM_Classifier.py
+++ 30_Machine_Learning/ML035_DeepSVM_Classifier.py
@@ -116,84 +116,6 @@ pad_series_torch = torch.FloatTensor(pad_series)
 import torch.nn as nn
 import torch.optim as optim
 
-# class Distance(nn.Module):
-#     def __init__(self, Sigma=None, boundary=None, kernel='gaussian'):
-#         super().__init__()
-        
-#         self.Sigma = nn.Parameter(torch.rand(1)) if Sigma is None else Sigma
-#         self.boundary = nn.Parameter(torch.rand(1)) if boundary is None else boundary
-#         self.kernel = kernel
-    
-#     def _is_mat(self, p):
-#         return p is not None and getattr(p, "ndim", 0) == 2
-    
-#     def mahalanobis_dist(self, X, Y=None, Sigma=None):
-#         """pairwise distance: Euclidean (optional /sigma) or Mahalanobis (Sigma matrix)."""
-#         Y = X if Y is None else Y
-#         D = X.unsqueeze(-2) - Y.unsqueeze(-3)                # (B,n,m,d)
-
-#         if self._is_mat(Sigma):
-#             Sigma_inv = torch.linalg.inv(Sigma)
-#             s = (D @ Sigma_inv * D).sum(dim=-1) 
-#             # s =  torch.einsum("nmd,dd,nmd->nm", D, torch.linalg.inv(Sigma), D)
-#         else:
-#             Sigma = 1 if Sigma is None else Sigma
-#             s = (D*D).sum(dim=-1) / (Sigma**2)
-#             # s = torch.einsum("nmd,nmd->nm", D, D) / (Sigma**2)       # (D*D).sum(-1)/(Sigma**2)
-#         s =  torch.clamp(s, min=0.0)
-#         return torch.sqrt(s)
-
-#     def _u(self, X, Y=None, boundary=1.0):
-#         """normalized distance for compact kernels."""
-#         if self._is_mat(boundary):          # ellipsoid: already normalized, boundary at u<=1
-#             return self.mahalanobis_dist(X, Y, Sigma=boundary)
-#         return self.mahalanobis_dist(X, Y) / boundary
-
-#     def gaussian_kernel(self, X, Y=None, Sigma=1.0):
-#         d = self.mahalanobis_dist(X, Y, Sigma=Sigma)
-#         return torch.exp(-0.5 * d * d)
-
-#     def uniform_kernel(self, X, Y=None, boundary=1.0):
-#         u = self._u(X, Y, boundary)
-#         return (u <= 1).to(dtype=u.dtype)
-
-#     def linear_kernel(self, X, Y=None, boundary=1.0):
-#         u = self._u(X, Y, boundary)
-#         return torch.clamp(1 - u, 0, 1)
-
-#     def epanechnikov_kernel(self, X, Y=None, boundary=1.0):
-#         u = self._u(X, Y, boundary)
-#         return torch.clamp(1 - u*u, 0, 1)
-
-#     def quartic_kernel(self, X, Y=None, boundary=1.0):
-#         u = self._u(X, Y, boundary)
-#         t = torch.clamp(1 - u*u, 0, 1)
-#         return t*t
-
-#     def forward_kernel(self, X, Y=None, kernel=None, Sigma=None, boundary=None):
-#         Sigma = nn.functional.softplus(self.Sigma) if Sigma is None else Sigma
-#         boundary = nn.functional.softplus(self.boundary) if boundary is None else boundary
-#         kernel = self.kernel if kernel is None else kernel
-        
-#         if kernel == 'gaussian':
-#             return self.gaussian_kernel(X, Y, Sigma)
-#         elif kernel == 'uniform':
-#             return self.uniform_kernel(X, Y, boundary)
-#         elif kernel == 'linear':
-#             return self.linear_kernel(X, Y, boundary)
-#         elif kernel == 'epanechnikov':
-#             return self.epanechnikov_kernel(X, Y, boundary)
-#         elif kernel == 'quartic':
-#             return self.quartic_kernel(X, Y, boundary)
-
-#     def forward(self, X, Y=None, kernel=None, Sigma=None, boundary=None):
-#         dist_mat = self.forward_kernel(X, Y=Y, kernel=kernel, Sigma=Sigma, boundary=boundary)
-#         dist = torch.nansum(dist_mat, dim=-1, keepdim=True)
-#         return dist  
-
-################################################################################
-
-
 class Distance(nn.Module):
     def __init__(self, Sigma=None, boundary=None, kernel='gaussian', valid_masking=True):
         super().__init__()
@@ -320,18 +242,13 @@ class Distance(nn.Module):
 dist = Distance(Sigma=torch.tensor(2.25), kernel='gaussian')
 # dist = Distance( boundary=torch.tensor(4.0), kernel='linear')
 
-# a = torch.rand(8,2)
-# a[-3:,:] = -1
-# b = torch.rand(7,2)
-# b[-2:,:] = -1
-# dist.forward_kernel(a,b)
-# dist.forward_kernel(a)
-# dist.forward(a,b)
+
 
 dist_mat = dist.forward_kernel(pad_series_torch[:,:,-2:])
 pd.DataFrame(dist_mat[3].numpy().round(1)).to_clipboard()
 intensives = dist.forward(pad_series_torch[:,:,-2:]).squeeze(-1)
 max_intensives = intensives.max(dim=-1)[0].numpy()
+
 
 
 
@@ -345,7 +262,6 @@ plt.hist(max_intensives, bins=30, edgecolor='gray', alpha=0.5)
 plt.show()
 
 # --------------------------------------------------------------------------------
-n_defect[:10]
 # Visualize
 xx, yy = np.meshgrid(np.linspace(0, 1, 50), np.linspace(0, 1, 50))
 grid = np.c_[xx.ravel(), yy.ravel()]  # (40000, 2)
@@ -391,7 +307,9 @@ label_true = ((max_intensives) > threshold).astype(int)
 label_obs = ((max_intensives + noise) > threshold).astype(int)
 
 label_torch = torch.LongTensor(label_obs)
-print(torch.unique(label_torch, return_counts=True))
+num_classes = torch.unique(label_torch, return_counts=True)[1]
+print(num_classes)
+
 
 
 # --------------------------------------------------------------------------------
@@ -399,10 +317,15 @@ from torch.utils.data import TensorDataset, DataLoader
 train_dataset = TensorDataset(pad_series_torch[:,:,-2:], label_torch)
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
+# x = pad_series_torch[[5, 11],:20,-2:]
+
+
+
 
 
 ##########################################################################################
-
+# 【 Deep SVM Kernel Model 】#############################################################
+##########################################################################################
 import torch.nn as nn
 import torch.optim as optim
 
@@ -422,17 +345,19 @@ class KernelModel(nn.Module):
         self.dist_layer = Distance(kernel='gaussian')
         # self.dist_layer = Distance(kernel='linear')
     
-    def forward(self, X):
+    def forward_intensive(self, X, Y=None):
         # self.Sigma = nn.functional.softplus(self.params_Sigma) 
         self.Sigma_vec = nn.functional.softplus(self.params_Sigma_vec) 
         self.Sigma = self.Sigma_vec.diag()
         self.boundary = nn.functional.softplus(self.params_boundary) 
         self.threshold = nn.functional.softplus(self.param_threhold)
         
-        
-        
-        # intensive = self.dist_layer(X, Sigma=torch.tensor(1.5))
-        intensive = self.dist_layer(X, Sigma=self.Sigma)
+        # intensive = self.dist_layer(X, Y, Sigma=torch.tensor(1.5))
+        intensive = self.dist_layer(X, Y, Sigma=self.Sigma)
+        return intensive
+    
+    def forward(self, X):
+        intensive = self.forward_intensive(X)
         # intensive = self.dist_layer(X, boundary=self.boundary)
         max_intensive, _ = intensive.squeeze(-1).max(dim=-1, keepdim=True)
         
@@ -445,26 +370,43 @@ model = KernelModel()
 f"{sum(p.numel() for p in model.parameters() if p.requires_grad):,}"
 
 
+
 # def squared_hingeloss(y_pred, y_true, margin=1):
-#     return torch.clamp(margin - y_true * y_pred, min=0) ** 2
+#     return torch.clamp(margin - (y_true*2-1) * y_pred, min=0) ** 2
+class BatchSquareHingeLoss():
+    def __init__(self, num_classes=None):
+        self.weights = None
+        if num_classes is not None:
+            weights = num_classes.flip(0)
+            self.weights = weights / weights.sum()
+    
+    def loss_function(self, model, batch, optiimizer=None):
+        batch_X, batch_y = batch
+        batch_y_pn = batch_y*2 - 1
+        y_pred = model.forward(batch_X)
+        
+        unique_class = batch_y.unique()
+        if len(unique_class) > 1:
+            weights = self.weights if self.weights is not None else torch.ones(2)/2
+        else:
+            weights = torch.zeros(2)
+            weights[unique_class] = 1
+        
+        hinge_loss = torch.tensor(0.0)
+        if len(batch_y_pn[batch_y_pn < 0]) > 0:   # negative_loss
+            hinge_loss += weights[0] * (torch.clamp(1 - batch_y_pn[batch_y_pn < 0] * y_pred[batch_y_pn < 0], min=0) ** 2).mean()
+            
+        if len(batch_y_pn[batch_y_pn > 0]) > 0:     # positive_loss
+            hinge_loss += weights[1] * (torch.clamp(1 - batch_y_pn[batch_y_pn > 0] * y_pred[batch_y_pn > 0], min=0) ** 2).mean()
+        
+        return hinge_loss
 
-def loss_function(model, batch, optimizer=None):
-    batch_X, batch_y = batch
-    
-    batch_y_pn = batch_y*2 - 1
-    y_pred = model.forward(batch_X)
-    # hinge_loss = (torch.clamp(1 - batch_y_pn * y_pred, min=0) ** 2).mean()
-    
-    loss_pos = (torch.clamp(1 - batch_y_pn[batch_y_pn > 0] * y_pred[batch_y_pn > 0], min=0) ** 2).mean()
-    loss_neg = (torch.clamp(1 - batch_y_pn[batch_y_pn < 0] * y_pred[batch_y_pn < 0], min=0) ** 2).mean()
-
-    hinge_loss = 0.5 * (loss_pos + loss_neg)
-    return hinge_loss
-    
+# squared_hinge_loss = BatchSquareHingeLoss(num_classes)  # num_classes
+squared_hinge_loss = BatchSquareHingeLoss()  # num_classes
 
 tm = TorchModeling(model)
-tm.compile(optimizer=optim.AdamW(model.parameters(), lr=3e-4)
-           ,loss_function=loss_function)
+tm.compile(optimizer=optim.AdamW(model.parameters(), lr=1e-3)
+           ,loss_function=squared_hinge_loss.loss_function)
 tm.train_model(train_loader, epochs=300)
 
 print(model.Sigma.detach(), model.threshold.detach())
@@ -487,10 +429,170 @@ print(classification_report(label_torch, label_true))   # true - obs
 print(classification_report(pred_label, label_true))    # pred - true
 
 
-# # 예: batch_y_pn ∈ {+1, -1}
-# pos_weight = N_total / (2 * N_pos)
-# neg_weight = N_total / (2 * N_neg)
 
-# weights = torch.where(batch_y_pn > 0, pos_weight, neg_weight)
 
-# hinge_loss = (weights * (torch.clamp(1 - batch_y_pn * y_pred, min=0) ** 2)).mean()
+
+
+
+
+
+##########################################################################################
+# 【 Convolutional Deep SVM Kernel Model 】###############################################
+##########################################################################################
+import torch.nn as nn
+import torch.optim as optim
+
+class ConvKernelModel(nn.Module):
+    def __init__(self, window_size=100, stride=50):
+        super().__init__()
+        self.window_size = window_size
+        self.stride = stride
+        
+        # self.params_Sigma =  nn.Parameter( torch.rand(()) )
+        self.params_Sigma_vec = nn.Parameter( torch.rand(2) )
+        self.params_boundary =  nn.Parameter( torch.rand(()) )
+        self.param_threhold = nn.Parameter(torch.rand(()))
+        
+        self.Sigma = None
+        self.Sigma_vec = None
+        self.boundary = None
+        self.threshold = None
+        self.dist_layer = Distance(kernel='gaussian')
+        # self.dist_layer = Distance(kernel='linear')
+    
+    def sort_sequence(self, X, padding_value=-1.0, pad_all_features=True, descending=False):
+            """
+             . X: (..., Seq, Feature) > 2D
+             . padding_value: 패딩 값 (예: -1)
+             . pad_all_features: True  -> feature 중 하나라도 padding_value면 패딩으로 간주 (네 기존 로직)
+                                False -> feature 전부가 padding_value일 때만 패딩으로 간주 (보통 패딩 벡터용)
+            """
+            seq_dim = -2      # (..., Seq, Feature)에서 Seq 축
+            feat_dim = -1     # Feature 축
+
+            # padding mask: (..., Seq)
+            if pad_all_features:
+                pad_mask = (X == padding_value).any(dim=feat_dim)
+            else:
+                pad_mask = (X == padding_value).all(dim=feat_dim)
+
+            # 거리: (..., Seq)
+            dist = torch.norm(X, dim=feat_dim)
+
+            # 패딩을 뒤로 보내기 위한 score 조정
+            #    - descending=False(가까운 순): 패딩은 +inf
+            #    - descending=True (먼 순):   패딩은 -inf
+            fill_value = float('-inf') if descending else float('inf')
+            dist_masked = dist.masked_fill(pad_mask, fill_value)
+
+            # 정렬 인덱스: (..., Seq)
+            idx = torch.argsort(dist_masked, dim=feat_dim, descending=descending)
+
+            # gather를 위한 인덱스 확장: (..., Seq, Feature)
+            idx_expanded = idx.unsqueeze(feat_dim).expand(*X.shape[:-1], X.size(feat_dim))
+
+            # Seq 축으로 gather
+            X_sorted = torch.gather(X, dim=seq_dim, index=idx_expanded)
+
+            return X_sorted
+    
+    def forward_intensive_window(self, X, Y=None):
+        # self.Sigma = nn.functional.softplus(self.params_Sigma) 
+        self.Sigma_vec = nn.functional.softplus(self.params_Sigma_vec) 
+        self.Sigma = self.Sigma_vec.diag()
+        self.boundary = nn.functional.softplus(self.params_boundary) 
+        self.threshold = nn.functional.softplus(self.param_threhold)
+        
+        seq_len = X.shape[-2]
+        X_sorted = self.sort_sequence(X)
+        
+        n_window = 1 if seq_len < self.window_size else (seq_len - self.window_size + self.stride-1)//self.stride + 1
+        
+        # windowing forward
+        intensives = []
+        # max_intensives = torch.full((*X_sorted.shape[:-2], seq_len, 1), 0.0, device=X.device, dtype=X.dtype)
+        for i in range(n_window):
+            i_start, i_end = i*self.stride, i*self.stride+self.window_size
+            # print(i_start, i_end)
+            X_window = X_sorted[...,i_start:i_end,:]
+            intensive_window = self.dist_layer(X_window, Y, Sigma=self.Sigma)
+            # intensive_window = self.dist_layer(X_window, Y, boundary=self.boundary)
+        
+            intensives.append(intensive_window)
+            # cur_intensives = max_intensives[..., i_start:i_end, :].clone()
+            # max_intensives[..., i_start:i_end, :] = torch.maximum(cur_intensives, intensive_window)
+        intensives_cat = torch.concat(intensives, dim=-2)
+        return intensives_cat
+    
+    def forward(self, X):       
+        intensives_cat = self.forward_intensive_window(X)
+        
+        max_intensive, _ = intensives_cat.squeeze(-1).max(dim=-1, keepdim=True)
+        output_window = max_intensive - self.threshold
+        return output_window
+
+
+model = ConvKernelModel(window_size=50, stride=20)
+# model(pad_series_torch[:,:,-2:])
+# f"{sum(p.numel() for p in model.parameters() if p.requires_grad):,}"
+
+class BatchSquareHingeLoss():
+    def __init__(self, num_classes=None):
+        self.weights = None
+        if num_classes is not None:
+            weights = num_classes.flip(0)
+            self.weights = weights / weights.sum()
+    
+    def loss_function(self, model, batch, optiimizer=None):
+        batch_X, batch_y = batch
+        batch_y_pn = batch_y*2 - 1
+        y_pred = model.forward(batch_X)
+        
+        unique_class = batch_y.unique()
+        if len(unique_class) > 1:
+            weights = self.weights if self.weights is not None else torch.ones(2)/2
+        else:
+            weights = torch.zeros(2)
+            weights[unique_class] = 1
+        
+        hinge_loss = torch.tensor(0.0)
+        if len(batch_y_pn[batch_y_pn < 0]) > 0:   # negative_loss
+            hinge_loss += weights[0] * (torch.clamp(1 - batch_y_pn[batch_y_pn < 0] * y_pred[batch_y_pn < 0], min=0) ** 2).mean()
+            
+        if len(batch_y_pn[batch_y_pn > 0]) > 0:     # positive_loss
+            hinge_loss += weights[1] * (torch.clamp(1 - batch_y_pn[batch_y_pn > 0] * y_pred[batch_y_pn > 0], min=0) ** 2).mean()
+        
+        return hinge_loss
+
+torch.autograd.set_detect_anomaly(True)
+# squared_hinge_loss = BatchSquareHingeLoss(num_classes)  # num_classes
+squared_hinge_loss = BatchSquareHingeLoss()  # num_classes
+
+tm = TorchModeling(model)
+tm.compile(optimizer=optim.AdamW(model.parameters(), lr=1e-3)
+           ,loss_function=squared_hinge_loss.loss_function)
+tm.train_model(train_loader, epochs=100)
+
+print(model.Sigma.detach(), model.threshold.detach())
+# print(model.Sigma.detach(), model.threshold.detach())
+# print(model.Precision.detach(), model.threshold.detach())
+# print(model.boundary.detach(), model.threshold.detach())
+
+
+# ----------------------------------------------------------------------
+pred = model(pad_series_torch[:,:,-2:]).detach().to('cpu').numpy()
+pred_label = ((np.sign(pred)+1)/2).ravel()
+
+from sklearn.metrics import confusion_matrix, classification_report
+confusion_matrix(pred_label, label_torch)   # pred - obs
+confusion_matrix(label_true, label_torch)   # true - obs
+confusion_matrix(pred_label, label_true)    # pred - true
+
+print(classification_report(pred_label, label_torch))   # pred - obs
+print(classification_report(label_torch, label_true))   # true - obs
+print(classification_report(pred_label, label_true))    # pred - true
+
+
+
+
+
